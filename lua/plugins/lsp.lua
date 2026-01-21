5048f1ce1c3e74aaf89e472a1ca0a8baee000343
Hana1015 <hannari1015@gmail.com>
2026-01-21 15:23:32 +0900

    mason ensure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,5 +31,13 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "r_language_server",
+        "bashls",
+      },
+    },
   },
 }
